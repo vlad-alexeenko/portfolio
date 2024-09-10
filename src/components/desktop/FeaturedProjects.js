7b596a9b19cabf1d47e0c1bd7407d2ce4d034df5
Vlad Alexeenko <valexeenko74@gmail.com>
2024-09-09 17:23:56 -0700

    adding sharpcom to projects

--- src/components/desktop/FeaturedProjects.js
+++ src/components/desktop/FeaturedProjects.js
@@ -15,9 +15,9 @@ const FeaturedProjects = () => {
                 <Header>Featured projects:</Header>
                 <Description>I have worked on many projects while being a Software Engineer in Test, here are a few of my live, real-world projects:</Description>
                 <Flex>
-                <Project text="Opportunity@Work" text2="Functional, Integration and Regression testing for every new ticket (Cypress.io & JS). I brought and implemented great standards of test plans and reports for regression testing coverage for that project." link="https://www.stellarworx.org/" imageSrc={Stellarworx} altText="Stellarworx logo"/>
-                <Project text="Cubionic" text2="Frontend development (React & TS), API testing, Functional and Smoke testing. On that project, I created a Postman collection for API automation testing & interaction. Performed frontend development tasks." link="https://cubionic.com/" imageSrc={Cubionic} altText="Stellarworx logo"/>
-                <Project text="My Tablo" text2="Functional, API testing. Smoke and Regression testing (Appium & JS). I used my knowledge of equivalence classes for regression coverage that helped us save time and money with the same high level of quality." link="https://www.outsetmedical.com/" imageSrc={MyTablo} altText="Stellarworx logo"/>
+                <Project text="Sharp HealthCare" text2="Functional testing. Smoke, Regression and API testing (Cypress & TS). I automated DCT manual test cases, including critical test scenarios, and implemented modern design patterns in testing." link="https://www.sharp.com/" imageSrc={MyTablo} altText="Sharp HealthCare logo"/>    
+                <Project text="Opportunity@Work" text2="Functional, Integration and Regression testing for every new ticket (Cypress & JS). I brought and implemented great standards of test plans and reports for regression testing coverage for that project." link="https://www.stellarworx.org/" imageSrc={Stellarworx} altText="Stellarworx logo"/>
+                <Project text="Cubionic" text2="Frontend development (React & TS), API testing, Functional and Smoke testing. On that project, I created a Postman collection for API automation testing & interaction. Performed frontend development tasks." link="https://cubionic.com/" imageSrc={Cubionic} altText="Cubionic logo"/>
                 </Flex>
             </AboutMe>
         </MainBlock>
